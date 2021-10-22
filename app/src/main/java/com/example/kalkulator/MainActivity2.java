3225aded74b4010fa468ae079e9dd37eea1066b5
Willy Novianto <willy.novianto78@gmail.com>
2021-10-22 21:52:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.kalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void Next(View view){
+        Intent intent=new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
